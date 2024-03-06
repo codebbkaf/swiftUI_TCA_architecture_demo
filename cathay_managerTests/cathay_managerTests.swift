55053776b8458d8c8ec7e3e340e0d78c8b37dc94
redtsenghahow <redtseng@hahow.in>
2024-03-06 16:31:26 +0800

    feat: add LoginReducer unit tests

--- cathay_managerTests/cathay_managerTests.swift
+++ cathay_managerTests/cathay_managerTests.swift
@@ -7,30 +7,34 @@
 
 import XCTest
 @testable import cathay_manager
+import ComposableArchitecture
 
-final class cathay_managerTests: XCTestCase {
+final class UnitTexts: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    @MainActor
+    func testLoginFlow() async {
+        let store = TestStore(initialState: LoginReducer.State()) {
+            LoginReducer()
+        }
+        
+        await store.send(.emailChanged("test@example.com")) {
+            $0.email = "test@example.com"
+        }
+        await store.receive(.statusChange(.idle))
+        await store.send(.passwordChanged("password123")) {
+            $0.password = "password123"
+        }
+        await store.send(.loginButtonTapped) {
+            $0.loginStatus = .loading
+        }
+        await store.receive(.statusChange(.error), timeout: Duration.seconds(2)) {
+            $0.loginStatus = .error
+        }
+        await store.send(.passwordChanged("password456")) {
+            $0.password = "password456"
+        }
+        await store.receive(.statusChange(.idle)) {
+            $0.loginStatus = .idle
         }
     }
-
 }
